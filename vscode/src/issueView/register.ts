1135a16547c108edde75cc382853b886afb7d31f
Radoslaw Szwajkowski <rszwajko@redhat.com>
2024-12-06 21:06:36 +0100

    Add issue tree view - part 1
    1. group incidents by message (top level) and file (2nd level)
    2. within message group the files are sorted based on the entire path
    3. within file group the incidents are sorted based on line number
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

---
+++ vscode/src/issueView/register.ts
@@ -0,0 +1,33 @@
+import { ExtensionState } from "src/extensionState";
+import * as vscode from "vscode";
+import { IssuesTreeDataProvider } from "./issueModel";
+import { Immutable } from "immer";
+import { ExtensionData, RuleSet } from "@editor-extensions/shared";
+
+export function registerIssueView({
+  extensionContext: context,
+  issueModel: model,
+}: ExtensionState): (data: Immutable<ExtensionData>) => void {
+  const provider = new IssuesTreeDataProvider(model);
+  vscode.window.registerTreeDataProvider("konveyor.issueView", provider);
+  const treeView = vscode.window.createTreeView<unknown>("konveyor.issueView", {
+    treeDataProvider: provider,
+    showCollapseAll: true,
+  });
+
+  treeView.message = model.message;
+  context.subscriptions.push(treeView);
+
+  provider.onDidChangeTreeData(() => {
+    treeView.message = model.message;
+  });
+
+  let lastRuleSets: Immutable<RuleSet[]> = [];
+  return (data: Immutable<ExtensionData>) => {
+    // by-reference comparison assumes immutable state object
+    if (lastRuleSets !== data.ruleSets) {
+      model.updateIssues(data.ruleSets);
+      lastRuleSets = data.ruleSets;
+    }
+  };
+}
